4008bf09f2d0e3a3f399bbc5e9c320b767c20231
markfirmware <markfirmware@users.noreply.github.com>
2017-06-29 02:00:43 -0400

    1.3.363

--- monitor/burst.sh
+++ monitor/burst.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 URL=http://163.172.178.31:9980  # Ultima Paris Xenial
 URL=http://45.32.223.179:5570   # webstatusprogram Atlanta
@@ -7,26 +7,23 @@ URL=http://104.156.232.107:5570 # webstatusprogram Sydney
 URL=http://108.61.117.135:5570  # webstatusprogram Amsterdam
 URL=http://163.172.163.75:9980  # Ultima Paris Jessie
 URL=http://45.79.200.166:5578   # webstatusprogram markfirmare Atlanta
-URL=http://localhost:5588   # webstatusprogram markfirmare Atlanta
+URL=http://localhost:5588         # webstatusprogram markfirmare Atlanta
 
 EPISODE=$(date +%H%M%S)
-CURLTIMEOUT=20
+GETTIMEOUT=30
 INTERVAL=$1
 BURSTCOUNT=$2
 COUNTER=0
-rm -f errors.txt
-clear
+rm -f wget.log.*
+rm -f wget.file.*
+echo -n 'starting   ' $BURSTCOUNT ' ... '
 while [[ $COUNTER -lt $BURSTCOUNT ]]
 do
-    FILE=threadlist.$EPISODE.$COUNTER
-    curl -m $CURLTIMEOUT $URL/status/about > $FILE 2>> errors.txt &
+    FILE=wget.$EPISODE.$COUNTER
+    wget --read-timeout=$GETTIMEOUT -a wget.log.$COUNTER -O wget.file.$COUNTER $URL/status &
     sleep $INTERVAL
     let COUNTER=COUNTER+1
 done
-time wait
-echo   'started    ' $BURSTCOUNT
-echo -n timeout
-grep 'curl: (28)' errors.txt | wc
-echo -n refused
-grep 'curl: (7)' errors.txt | wc
-grep curl errors.txt | grep -v '(28)' | grep -v '(7)'
+echo started
+wait
+grep "awaiting response" wget.log.* | grep -iv "200 OK"
